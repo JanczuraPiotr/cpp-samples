b8dea9dd4dc3f0a4cffa65a7b0a2dfe87574f025
JanczuraPiotr <piotr@janczura.pl>
2020-08-07 21:59:37 +0200

    interfejs

---
+++ interface/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by piotr@janczura.pl on 2020.08.07
+//
+
+#include <tuple>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::ignore = argc;
+    std::ignore = argv;
+
+    std::cout << "interface" << std::endl;
+}
