bd715d0ca60aad47079d0add1dc79131d23bf445
Piotr Janczura <piotr@janczura.pl>
2024-07-30 21:09:47 +0200

    Formatowanie

--- boost-path/main.cpp
+++ boost-path/main.cpp
@@ -10,10 +10,11 @@
 
 int main()
 {
-  boost::filesystem::path path{"/home/piotr/plik.cos"};
+    boost::filesystem::path path{"/home/piotr/plik.cos"};
 
-  std::cout << "relative_path :.. " << path.relative_path() << std::endl;
-  std::cout << "parent_path :.... " << path.parent_path() << std::endl;
-  std::cout << "filename :....... " << path.filename() << std::endl;
-  std::cout << "stem :........... " << path.stem() << std::endl;
-  std::cout << "extension :...... " << path.extension() << std::endl;
+    std::cout << "relative_path :.. " << path.relative_path() << std::endl;
+    std::cout << "parent_path :.... " << path.parent_path() << std::endl;
+    std::cout << "filename :....... " << path.filename() << std::endl;
+    std::cout << "stem :........... " << path.stem() << std::endl;
+    std::cout << "extension :...... " << path.extension() << std::endl;
+}
