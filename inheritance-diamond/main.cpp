53aa42887988ebcf4585ae9b289c30a2ef47e9da
Piotr Janczura <piotr@janczura.pl>
2024-09-12 15:12:04 +0200

    inheritance-diamound

--- inheritance-diamond/main.cpp
+++ inheritance-diamond/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 
 #include "../Logger.hpp"
@@ -6,11 +5,11 @@
 class C0 {
 public:
 
-    C0() : _i(0) {                                                          logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    C0() : _i(0) {                                                          logger.log(__PRETTY_FUNCTION__, __LINE__);
 
     };
 
-    virtual ~C0() {                                                         logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    virtual ~C0() {
 
     };
 
@@ -18,7 +17,7 @@ public:
         return _i;
     }
 
-    virtual void i(int i) {                                                 logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    virtual void i(int i) {                                                 logger.log(__PRETTY_FUNCTION__, __LINE__);
         _i = i;
     }
 
@@ -28,61 +27,60 @@ private:
 };
 
 
-
+// Zobacz różnicę.
+// class C1 : public C0 {
 class C1 : virtual public C0 {
 public:
-    C1() {                                                                  logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    C1() {                                                                  logger.log(__PRETTY_FUNCTION__, __LINE__);
 
     };
 
-    ~C1() override {                                                        logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    virtual void v() {                                                      logger.log(__PRETTY_FUNCTION__, __LINE__);
 
-    };
-
+    }
 };
 
+// Zobacz różnicę.
+// class C2 : public C0 {
 class C2 : virtual public C0 {
 public:
-    C2() {                                                                  logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    C2() {                                                                  logger.log(__PRETTY_FUNCTION__, __LINE__);
 
     };
 
-    ~C2() override {                                                        logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    virtual void v() {                                                      logger.log(__PRETTY_FUNCTION__, __LINE__);
 
-    };
-
+    }
 };
-
-// Usunięcię virtual z: "virtual public C0" przy definicji jednej z klas C1 lib C2 powoduje błąd w
-// wierszach 72, 74 i 81.
 
 class C3 : public C1, public C2 {
 public:
-    C3() : C1(), C2() {                                                     logger.log(__PRETTY_FUNCTION__ , __LINE__);
+    C3() : C1(), C2() {                                                     logger.log(__PRETTY_FUNCTION__, __LINE__);
 
-    }
-
-    void v() {                                                    logger.log(__PRETTY_FUNCTION__ , __LINE__);
-        logger.log({{"X::i()"}, {std::to_string(C1::i())}}, __LINE__);
-        logger.log({{"Y::i()"}, {std::to_string(C2::i())}}, __LINE__);
-
-        logger.log({{"i()"}, {std::to_string(i())}}, __LINE__);
-
-        i(1);
-        C1::i(2);
-        C2::i(3);
-
-        logger.log({{"X::i()"}, {std::to_string(C1::i())}}, __LINE__);
-        logger.log({{"Y::i()"}, {std::to_string(C2::i())}}, __LINE__);
-
-        logger.log({{"i()"}, {std::to_string(i())}}, __LINE__);
     }
 };
 
 int main()
 {
-
     logger.line(__PRETTY_FUNCTION__ , __LINE__);
     C3 c3;
-    c3.v();
+    c3.i(1);
+    logger.log({{"i"}, {std::to_string(c3.i())}}, __LINE__);
+    // c3.v(); !!!
+    {
+        logger.line(__PRETTY_FUNCTION__, __LINE__);
+        C3 *pc3 = new C3();
+        pc3->i(2);
+        logger.log({{"i"}, {std::to_string(pc3->i())}}, __LINE__);
+        // pc3->v(); !!!
+        delete pc3;
+    }
+    {
+        logger.line(__PRETTY_FUNCTION__, __LINE__);
+        C2 *pc2 = new C2();
+        pc2->i(3);
+        logger.log({{"i"}, {std::to_string(pc2->i())}}, __LINE__);
+        pc2->v();
+        delete pc2;
+    }
 }
