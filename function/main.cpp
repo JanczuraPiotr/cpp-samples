992381d84ac164061816c53567afa9b85d6a82d1
Piotr Janczura <piotr@janczura.pl>
2024-11-04 16:08:37 +0100

    Binding functions of std::function types with class methods.

--- function/main.cpp
+++ function/main.cpp
@@ -5,6 +5,8 @@
 #include "../Logger.hpp"
 
 typedef std::function<void(void)> FVoidVoid;
+typedef std::function<int(int)> FIntInt;
+typedef std::vector<FIntInt> VFIntInt;
 
 class C1 {
 public:
@@ -25,12 +27,21 @@ private:
     FVoidVoid _fVoidVoid;
 };
 
+class C2 {
+public:
+    C2() = default;
+
+    void callback() { logger.log(__PRETTY_FUNCTION__ , __LINE__); }
+    int callback(int i) { logger.log(__PRETTY_FUNCTION__ , __LINE__); return i;}
+
+};
 
 
 int main()
 {
-    FVoidVoid fVoidVoid;
     {
+        FVoidVoid fVoidVoid;
+
         try {
             fVoidVoid();
         } catch (std::bad_function_call &ex) {
@@ -38,25 +49,31 @@ int main()
         } catch (...) {
             logger.log("Unexpected exception.", __LINE__);
         }
-    }
-    logger.log("--------------",__LINE__);
 
-    {
-        auto namedLambda = [] () { logger.log("Named lambda", __LINE__); };
+        logger.log("--------------", __LINE__);
+        auto namedLambda = []() { logger.log("Named lambda", __LINE__); };
         fVoidVoid = namedLambda;
         fVoidVoid();
-    }
 
-    {
+        logger.log("--------------", __LINE__);
+
         C1 c1(fVoidVoid);
         c1.call();
         c1.replace([] () { logger.log("Anonymous lambda.", __LINE__); });
         c1.call();
+
+        logger.log("--------------",__LINE__);
+
+        C2 c2;
+        fVoidVoid = std::bind(static_cast<void(C2::*)(void)>(&C2::callback), c2);
+        fVoidVoid();
+
+        FIntInt fIntInt;
+        fIntInt = std::bind(static_cast<int(C2::*)(int)>(&C2::callback), c2, std::placeholders::_1);
+        logger.log({"fIntInt(3)", std::to_string(fIntInt(3))}, __LINE__);
     }
 
     logger.log("--------------",__LINE__);
-    typedef std::function<int(int)> FIntInt;
-    typedef std::vector<FIntInt> VFIntInt;
     {
         VFIntInt vIntInt;
         for (int ix = 0; ix < 5; ++ix) {
